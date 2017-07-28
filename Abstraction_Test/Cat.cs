5ba7e721208d24e159fa4c89179803918d09c4b0
Rashmi <rashmi.suresh@tismotech.com>
2017-07-28 13:04:22 +0530

    Add project files.

---
+++ Abstraction_Test/Cat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Abstraction_Test
+{
+     public abstract class Cat
+        {
+            public abstract void Eat();
+            public abstract void Hunt();
+            public abstract void Sleep();
+        }
+   
+}
---
+++ Abstraction_Test/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Abstraction_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public class Lion : Cat
+        {
+            public Lion(ColorSpectrum color)
+            {
+                LionColor = color.ToString();
+            }
+            public enum ColorSpectrum { Brown, White }
+            public string LionColor { get; set; }
+            public override void Eat()
+            {
+                Console.WriteLine($"The {LionColor} lion eats.");
+            }
+
+            public override void Hunt()
+            {
+                Console.WriteLine($"The {LionColor} lion hunts.");
+            }
+
+            public override void Sleep()
+            {
+                Console.WriteLine($"The {LionColor} lion sleeps.");
+            }
+        }
+    }
+}
